923b6d815cba00f50d0f60806dcc7893cdddeff8
khpallon <karlhannespallon@gmail.com>
2024-05-18 22:08:13 +0000

    made starting point to player location

--- finder.js
+++ finder.js
@@ -1,9 +1,47 @@
 let numberList = [];
 let answer;
+let playerLocation = [75, 76, 77, 78]
+let numLoc;
 
 function main(){
     document.getElementById("minigame").innerHTML = "";
     randomize();
+    movement();
+    setInterval(update, 4000);
+
+
+}
+
+function movement(){
+
+    window.addEventListener("keydown", (event) => {
+        // UP
+        if (event.keyCode === 38) {
+            console.log('yo')
+          for (let i = 0; i < playerLocation.length; i++){
+            let element = document.getElementsByClassName("chars")[playerLocation[i]];
+            element.style.color = "black";
+            element.classList.remove('green')
+            playerLocation[i] = playerLocation[i] - 10;
+            element.classList.add('green');
+
+
+          }
+        }
+        // DOWN
+        if (event.keyCode === 40) {
+            return;
+          }
+        // LEFT
+        if (event.keyCode === 37) {
+            return;
+          }
+        // RIGHT
+        if (event.keyCode === 39) {
+            return;
+          }
+      });
+
 }
 
 function randomize(){
@@ -11,9 +49,18 @@ function randomize(){
         let combo = Math.floor(Math.random() * 9).toString() + Math.floor(Math.random() * 9).toString();
         numberList.push(combo); 
 
-        var template = htmlToElement(`<div class="chars">${combo}</div>`)
+        if (i > (playerLocation[0]-1) && i < (playerLocation[3]+1)){
+            var template = htmlToElement(`<div class="chars green">${combo}</div>`)
+        } else {
+            var template = htmlToElement(`<div class="chars">${combo}</div>`)
+        }
+
+        
         document.getElementById("minigame").appendChild(template);
     }
+
+    
+
 
     let start = Math.floor(Math.random() * 136) 
 
@@ -21,14 +68,7 @@ function randomize(){
 
     console.log(answer)
 
-
-
     console.log(numberList.length)
-
-
-
-    setInterval(update, 4000);
-    
 }
 
 function update(){
@@ -36,7 +76,12 @@ function update(){
     document.getElementById("minigame").innerHTML = "";
 
     for (let i = 0; i < numberList.length ; i++){
-        var template = htmlToElement(`<div class="chars">${numberList[i]}</div>`)
+
+        if (i > (playerLocation[0]-1) && i < (playerLocation[3]+1)){
+            var template = htmlToElement(`<div class="chars green">${numberList[i]}</div>`)
+        } else {
+            var template = htmlToElement(`<div class="chars">${numberList[i]}</div>`)
+        }
         document.getElementById("minigame").appendChild(template);
     }
 
