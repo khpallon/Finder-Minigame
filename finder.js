49cbdfd01728683bf07498e6b6015e0b1c50dc62
khpallon <karlhannespallon@gmail.com>
2024-05-20 17:14:54 +0000

    finished product

--- finder.js
+++ finder.js
@@ -1,26 +1,126 @@
 let numberList = [];
 let answer;
-let playerLocation = [64, 65, 66, 67]
+let playerLocation = [47, 48, 49, 50]
+let correct = [];
 let numLoc;
 let submit = "";
 let play;
+let gameTimer;
+let timeout;
+let choice = [];
+let alphabet = "abcdefghijklmnopqrstuvwxyz"
+let alphanum = "abcdefghijklmnopqrstuvwxyz0123456789"
+let number = "0123456789"
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let slider = document.getElementById('slider');
+    let value = document.getElementById('radialValue');
+
+    slider.addEventListener('input', () => {
+    value.textContent = "Time: " + slider.value;
+});
+});
+
 
 function main(){
-    randomize();
+    if (document.getElementById("alphabet").checked === true){
+        choice.push(alphabet)
+    }
+    if (document.getElementById("number").checked === true){
+        choice.push(number)
+    }
+    if (document.getElementById("alphanum").checked === true){
+        choice.push(alphanum)
+    }
+    randomize(choice);
     window.addEventListener("keydown", movement)
-    play = setInterval(update, 2000);
+    play = setInterval(update, 1500);
+    document.getElementById('timer').innerHTML = setTime;
+    gameTimer = setInterval(timer, 1000)
+
+    timeout = setTimeout(function(){
+        clearInterval(gameTimer)
+        document.getElementById('timer').innerHTML = 0;
+        failGame();
+    }, setTime * 1000)
 
 
+}
+
+function randomize(choice){
+    let symbol = Math.floor(Math.random() * choice.length);
+    var characters = choice[symbol]
+    console.log(choice)
+
+    for (let i = 0; i < 99 ; i++){
+        let first = Math.floor(Math.random() * characters.length);
+        let second = Math.floor(Math.random() * characters.length);
+
+        combo = characters[first] + characters[second]
+
+        numberList.push(combo); 
+
+        if (i > (playerLocation[0]-1) && i < (playerLocation[3]+1)){
+            var template = htmlToElement(`<div class="chars green ${i}">${combo.toUpperCase()}</div>`)
+        } else {
+            var template = htmlToElement(`<div class="chars ${i}">${combo.toUpperCase()}</div>`)
+        }
+
+        
+        document.getElementById("minigame").appendChild(template);
+    }
+
+    
+
+
+    let start = Math.floor(Math.random() * 96) 
+
+    answer = numberList[start] + " " + numberList[start+1] + " " + numberList[start+2] + " " + numberList[start+3]
+
+    correct = [start, start+1, start+2, start+3];
+    
+    document.getElementById("findthis").innerText = answer.toUpperCase()
+    document.getElementById("findthis").style.display = "inline-block"
+
+    answer = answer.replace(/ /g, "")
+}
+
+function failGame(){
+    for (let i = 0; i < playerLocation.length; i++){
+        let element = document.getElementsByClassName("chars")[playerLocation[i]];
+        let correctA = document.getElementsByClassName("chars")[correct[i]];
+        correctA.classList.add('correct')
+        element.classList.remove('green')
+        element.classList.add('red')
+
+        }
+        clearInterval(play)
+        clearInterval(gameTimer)
+        clearTimeout(timeout)
+        window.removeEventListener('keydown', movement)
+}
+
+function timer(){
+    setTime--
+    document.getElementById('timer').innerHTML = setTime;
 }
 
 function reset(){
+    setTime = document.getElementById('slider').value;
+    choice = [];
     document.getElementById("minigame").innerHTML = "";
     clearInterval(play)
+    clearInterval(gameTimer)
+    clearTimeout(timeout)
     numberList = []
-    playerLocation = []
+    playerLocation = [47, 48, 49, 50]
     submit = "";
+    
     main();
 }
+
+
 
 function movement(){
         if (event.keyCode === 13){
@@ -29,23 +129,18 @@ function movement(){
             let element = document.getElementsByClassName("chars")[playerLocation[i]].innerHTML;
                 submit += element
             }
-            console.log(submit)
-            console.log(answer)
             if (answer === submit){
-                console.log("YAAAAAAAAAAAA")
                 clearInterval(play)
+                clearInterval(gameTimer)
+                clearTimeout(timeout)
                 window.removeEventListener('keydown', movement)
+                for (let i = 0; i < playerLocation.length; i++){
+                    let element = document.getElementsByClassName("chars")[playerLocation[i]];
+                    element.classList.add("correct") 
+                }
 
             } else {
-                console.log("NAAAAAAAAAAAAAAA")
-                for (let i = 0; i < playerLocation.length; i++){
-                let element = document.getElementsByClassName("chars")[playerLocation[i]];
-                element.classList.remove('green')
-                element.classList.add('red')
-
-                }
-                clearInterval(play)
-                window.removeEventListener('keydown', movement)
+                failGame();
             }
         }
 
@@ -71,7 +166,6 @@ function movement(){
         }
         // DOWN
         if (event.keyCode === 40) {
-            console.clear();
 
             event.preventDefault();
             for (let i = 0; i < playerLocation.length; i++){
@@ -82,7 +176,6 @@ function movement(){
             } else {
                 playerLocation[i] = playerLocation[i] + 11;
             }
-            console.log(playerLocation[i])
             document.getElementsByClassName("chars")[playerLocation[i]].classList.add('green');
 
 
@@ -146,48 +239,27 @@ function movement(){
 
 }
 
-function randomize(){
-    for (let i = 0; i < 99 ; i++){
-        let combo = Math.floor(Math.random() * 9).toString() + Math.floor(Math.random() * 9).toString();
-        numberList.push(combo); 
 
-        if (i > (playerLocation[0]-1) && i < (playerLocation[3]+1)){
-            var template = htmlToElement(`<div class="chars green ${i}">${combo}</div>`)
-        } else {
-            var template = htmlToElement(`<div class="chars ${i}">${combo}</div>`)
-        }
-
-        
-        document.getElementById("minigame").appendChild(template);
-    }
-
-    
-
-
-    let start = Math.floor(Math.random() * 99) 
-
-    answer = numberList[start] + " " + numberList[start+1] + " " + numberList[start+2] + " " + numberList[start+3]
-    
-    document.getElementById("findthis").innerText = answer
-
-    answer = answer.replace(/ /g, "")
-}
 
 function update(){
     numberList.push(numberList.shift());
     document.getElementById("minigame").innerHTML = "";
 
-    
-
     for (let i = 0; i < numberList.length ; i++){
         if (playerLocation.includes(i)){
-            var template = htmlToElement(`<div class="chars green ${i}">${numberList[i]}</div>`)
+            var template = htmlToElement(`<div class="chars green ${i}">${numberList[i].toUpperCase()}</div>`)
         } else {
-            var template = htmlToElement(`<div class="chars ${i}">${numberList[i]}</div>`)
+            var template = htmlToElement(`<div class="chars ${i}">${numberList[i].toUpperCase()}</div>`)
         }
         document.getElementById("minigame").appendChild(template);
     }
 
+    for (var entry = 0; entry < correct.length; entry++){
+        if (correct[entry] === 0){
+            correct[entry] = 99;
+        }
+        correct[entry] = correct[entry]-1
+    }
 
 }
 
